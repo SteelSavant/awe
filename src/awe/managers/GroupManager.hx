9ad64b401a5f2821f988771275adccb6a36bfcf1
Tom Bebbington <tophattedcoder@gmail.com>
2017-07-04 05:18:09 +0100

    Update documentation

--- src/awe/managers/GroupManager.hx
+++ src/awe/managers/GroupManager.hx
@@ -1,12 +1,12 @@
 package awe.managers;
 
 import de.polygonal.ds.ArrayList;
-import awe.Manager;
+import awe.System;
 
 /**
 	Handles entity grouping.
 **/
-class GroupManager extends Manager {
+class GroupManager extends System {
 	var groups: Map<String, ArrayList<Entity>>;
 
 	public function new() {
@@ -15,28 +15,28 @@ class GroupManager extends Manager {
 	}
 
 	/**
-		Set the group of the entity.
-		@param entity The entity whose group is being set.
-		@param group The group to set the entity to.
-	**/
+	 *	Set the group of the entity.
+	 *	@param entity The entity whose group is being set.
+	 *	@param group The group to set the entity to.
+	 */
 	public function add(entity: Entity, group: String): Void {
 		if(!groups.exists(group))
 			groups.set(group, new ArrayList(8));
 		groups.get(group).add(entity);
 	}
 	/**
-		Get the entities contained in a given group.
-		@param group The group to check.
-		@return The entities.
-	**/
+	 *	Get the entities contained in a given group.
+	 *	@param group The group to check.
+	 *	@return The entities.
+	 */
 	public inline function getEntities(group: String): ArrayList<Entity>
 		return groups.get(group);
 
 	/**
-		Get all groups the entity belongs to..
-		@param entity The entity to get the groups of.
-		@return The groups it belongs to.
-	**/
+	 *	Get all groups the entity belongs to..
+	 *	@param entity The entity to get the groups of.
+	 *	@return The groups it belongs to.
+	 */
 	public function getGroups(entity: Entity): ArrayList<String> {
 		var contained = new ArrayList(8);
 		for(group in groups.keys()) {
