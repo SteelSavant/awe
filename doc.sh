3d29074a29d7e982aa06be7b9ae691f30025a5d6
Tom Bebbington <tophattedcoder@gmail.com>
2017-11-04 04:08:53 +0000

    Fix doc.sh so less messy

--- doc.sh
+++ doc.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 haxe doc.hxml
 haxelib run dox -i doc/doc.xml -o doc --title "Awe, the easy and fast ECS"
-git add -A .
-git commit -am "Update documentation"
+git add -A doc
+git commit -m "Update documentation"
 git subtree push --prefix doc origin gh-pages
+rm -rf doc
