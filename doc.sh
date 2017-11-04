c35072f00c2ee534b541663be9718b71a288f142
Tom Bebbington <tophattedcoder@gmail.com>
2017-11-04 04:23:47 +0000

    Update documentation

--- doc.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-haxe doc.hxml
-haxelib run dox -i doc/doc.xml -o doc --title "Awe, the easy and fast ECS"
-git add -A doc
-git commit -m "Update documentation"
-git subtree push --prefix doc origin gh-pages
-rm -rf doc
