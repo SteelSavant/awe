555846ae442d2cb846c50540da610f7ba390c9ca
Tom Bebbington <tombebb@protonmail.com>
2018-02-13 15:00:50 +0000

    Update dependencies, add AspectSubscriptionManager back

---
+++ src/awe/managers/AspectSubscriptionManager.hx
@@ -0,0 +1,9 @@
+package awe.managers;
+
+import de.polygonal.ds.ArrayList;
+import awe.EntitySubscription;
+
+class AspectSubscriptionManager extends System {
+    @:allow(awe)
+    var _subscriptions: ArrayList<EntitySubscription>;
+}
